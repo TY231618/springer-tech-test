0a2d750391fc95dca8d2bd34ee258c897173918f
Tony Young <ty_training@hotmail.co.uk>
2016-03-23 19:14:38 +0000

    Updated tests for private methods

--- spec/canvas_spec.rb
+++ spec/canvas_spec.rb
@@ -10,27 +10,27 @@ describe Canvas do
     end
 
     it 'checks length of the row' do
-      canvas = Canvas.new(6, 3)
-      expect(canvas.inside_area[0].size).to eq 3
+      canvas = Canvas.new(4, 20)
+      expect(canvas.inside_area[0].size).to eq 20
     end
   end
 
   context 'border' do
     it 'checks border sides' do
       canvas = Canvas.new(4, 20)
-      canvas.border_sides
-      expect(canvas.inside_area[0]).to eq "|"
+      canvas.display
+      expect(canvas.inside_area[1]).to include "|"
     end
 
     it 'checks border top' do
       canvas = Canvas.new(4, 20)
-      canvas.border_top_and_bottom
+      canvas.display
       expect(canvas.inside_area[0]).to include "-"
     end
 
     it 'checks border bottom' do
       canvas = Canvas.new(4, 20)
-      canvas.border_top_and_bottom
+      canvas.display
       expect(canvas.inside_area[-1]).to include "-"
     end
 
