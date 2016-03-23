25b354184163e1020c458ce72305267a8021521d
Tony Young <ty_training@hotmail.co.uk>
2016-03-23 19:21:08 +0000

    refactored tests

--- spec/canvas_spec.rb
+++ spec/canvas_spec.rb
@@ -2,36 +2,33 @@ require 'canvas'
 
 describe Canvas do
 
+  let(:subject) { described_class.new(4, 20) }
+
   context "initialize" do
 
     it 'checks height of the canvas' do
-      canvas = Canvas.new(4, 20)
-      expect(canvas.inside_area.size).to eq 4
+      expect(subject.inside_area.size).to eq 4
     end
 
     it 'checks length of the row' do
-      canvas = Canvas.new(4, 20)
-      expect(canvas.inside_area[0].size).to eq 20
+      expect(subject.inside_area[0].size).to eq 20
     end
   end
 
   context 'border' do
     it 'checks border sides' do
-      canvas = Canvas.new(4, 20)
-      canvas.display
-      expect(canvas.inside_area[1]).to include "|"
+      subject.display
+      expect(subject.inside_area[1]).to include "|"
     end
 
     it 'checks border top' do
-      canvas = Canvas.new(4, 20)
-      canvas.display
-      expect(canvas.inside_area[0]).to include "-"
+      subject.display
+      expect(subject.inside_area[0]).to include "-"
     end
 
     it 'checks border bottom' do
-      canvas = Canvas.new(4, 20)
-      canvas.display
-      expect(canvas.inside_area[-1]).to include "-"
+      subject.display
+      expect(subject.inside_area[-1]).to include "-"
     end
 
   end
